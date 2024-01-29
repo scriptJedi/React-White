beb2eff3b7f4add308016e545342f13b059fc67a
Dandaleon <workenplace@gmail.com>
2024-01-29 11:59:09 +0200

    Blocks 1-4 reworked, block 5 removed

--- src/blocks/Core.jsx
+++ src/blocks/Core.jsx
@@ -1,44 +1,43 @@
-import "../assets/styles/Block2.css";
+import "../assets/styles/Core.css";
 import CircleIcon from "../components/CircleIcon.jsx";
 
-const Block2 = () => {
+const Core = () => {
   return (
-    <section className="core">
+    <section className="core-de">
       <div className="container">
-        <div className="core-wrap">
-          <h5 className="core-title">HERE ARE SOME OF THE</h5>
-          <h2 className="core-subtitle">CORE FEATURES OF THE INSIDE</h2>
-          <div className="core-devider"></div>
-          <div className="core-block">
-            <div className="core-item">
-              <CircleIcon size={'small'} iconName="mobile-screen"/>
-              <p className="core-descr">
+        <div className="core-de__wrap">
+          <h5 className="core-de__title">HERE ARE SOME OF THE</h5>
+          <h2 className="core-de__subtitle">CORE FEATURES OF THE INSIDE</h2>
+          <div className="core-de-block">
+            <div className="core-de-block__item">
+              <CircleIcon size={"small"} iconName="mobile-screen" />
+              <p className="core-de-block__descr">
                 Cutting-edge Responsive Design: Our business solution boasts an
                 ultra-responsive layout that adapts seamlessly to all screens,
                 ensuring your website looks its best on any device.
               </p>
             </div>
-            <div className="core-item">
-              <CircleIcon size={'small'} iconName="gear"/>
-              <p className="core-descr">
+            <div className="core-de-block__item">
+              <CircleIcon size={"small"} iconName="gear" />
+              <p className="core-de-block__descr">
                 Endless Customization Possibilities: With hundreds of options
                 and variations to configure, you have the freedom to personalize
                 every aspect of your website to reflect your brand identity and
                 meet your business goals.
               </p>
             </div>
-            <div className="core-item">
-              <CircleIcon size={'small'} iconName="lightbulb"/>
-              <p className="core-descr">
+            <div className="core-de-block__item">
+              <CircleIcon size={"small"} iconName="lightbulb" />
+              <p className="core-de-block__descr">
                 Extensive Shortcodes Library: Enjoy access to a vast range of
                 shortcodes that simplify the creation of complex elements,
                 providing you with endless design possibilities and allowing you
                 to highlight your business offerings effortlessly.
               </p>
             </div>
-            <div className="core-item">
-              <CircleIcon size={'small'} iconName="sliders"/>
-              <p className="core-descr">
+            <div className="core-de-block__item">
+              <CircleIcon size={"small"} iconName="sliders" />
+              <p className="core-de-block__descr">
                 Versatile Portfolio Layouts: Showcase your work in style with
                 our multiple portfolio layout options. Whether you&apos;re a
                 creative agency or a professional service provider, you can
@@ -46,27 +45,27 @@ const Block2 = () => {
                 audience.
               </p>
             </div>
-            <div className="core-item">
-              <CircleIcon size={'small'} iconName="chart-pie"/>
-              <p className="core-descr">
+            <div className="core-de-block__item">
+              <CircleIcon size={"small"} iconName="chart-pie" />
+              <p className="core-de-block__descr">
                 Customizable Header Options: Make a lasting first impression
                 with our selection of multiple header layouts. With full
                 customization capabilities, you can tailor your header to create
                 a unique and impactful aesthetic that aligns with your brand.
               </p>
             </div>
-            <div className="core-item">
-              <CircleIcon size={'small'} iconName="cloud"/>
-              <p className="core-descr">
+            <div className="core-de-block__item">
+              <CircleIcon size={"small"} iconName="cloud" />
+              <p className="core-de-block__descr">
                 Blazing-Fast Performance: Our solution is optimized for
                 high-ranking page speed performance, ensuring your website loads
                 quickly and efficiently. Enjoy a seamless browsing experience
                 for your visitors, improving their engagement and satisfaction.
               </p>
             </div>
-            <div className="core-item">
-              <CircleIcon size={'small'} iconName="thumbs-up"/>
-              <p className="core-descr">
+            <div className="core-de-block__item">
+              <CircleIcon size={"small"} iconName="thumbs-up" />
+              <p className="core-de-block__descr">
                 Creative and Powerful Design: Stand out from the competition
                 with a design that is both visually compelling and strategically
                 powerful. Our solution offers a unique and creative design,
@@ -74,9 +73,9 @@ const Block2 = () => {
                 target audience.
               </p>
             </div>
-            <div className="core-item">
-              <CircleIcon size={'small'} iconName="mug-hot"/>
-              <p className="core-descr">
+            <div className="core-de-block__item">
+              <CircleIcon size={"small"} iconName="mug-hot" />
+              <p className="core-de-block__descr">
                 Businesses aim to create value and meet consumer needs. They
                 identify opportunities, build relationships, embrace technology,
                 and practice ethical conduct.
@@ -89,4 +88,4 @@ const Block2 = () => {
   );
 };
 
-export default Block2;
+export default Core;
